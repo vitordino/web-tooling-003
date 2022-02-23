5574dc5b5d1701a76479dbd93b269952c91665a7
Vitor Dino <ext.vdino@riotgames.com>
2022-02-23 17:21:48 +0100

    :sparkles: make dog app

--- src/App.js
+++ src/App.js
@@ -1,159 +1,50 @@
 import React, { useState } from "react";
+import useSWR from "swr";
 
-const DATA = {
-  message: {
-    affenpinscher: [],
-    african: [],
-    airedale: [],
-    akita: [],
-    appenzeller: [],
-    australian: ["shepherd"],
-    basenji: [],
-    beagle: [],
-    bluetick: [],
-    borzoi: [],
-    bouvier: [],
-    boxer: [],
-    brabancon: [],
-    briard: [],
-    buhund: ["norwegian"],
-    bulldog: ["boston", "english", "french"],
-    bullterrier: ["staffordshire"],
-    cattledog: ["australian"],
-    chihuahua: [],
-    chow: [],
-    clumber: [],
-    cockapoo: [],
-    collie: ["border"],
-    coonhound: [],
-    corgi: ["cardigan"],
-    cotondetulear: [],
-    dachshund: [],
-    dalmatian: [],
-    dane: ["great"],
-    deerhound: ["scottish"],
-    dhole: [],
-    dingo: [],
-    doberman: [],
-    elkhound: ["norwegian"],
-    entlebucher: [],
-    eskimo: [],
-    finnish: ["lapphund"],
-    frise: ["bichon"],
-    germanshepherd: [],
-    greyhound: ["italian"],
-    groenendael: [],
-    havanese: [],
-    hound: [
-      "afghan",
-      "basset",
-      "blood",
-      "english",
-      "ibizan",
-      "plott",
-      "walker",
-    ],
-    husky: [],
-    keeshond: [],
-    kelpie: [],
-    komondor: [],
-    kuvasz: [],
-    labradoodle: [],
-    labrador: [],
-    leonberg: [],
-    lhasa: [],
-    malamute: [],
-    malinois: [],
-    maltese: [],
-    mastiff: ["bull", "english", "tibetan"],
-    mexicanhairless: [],
-    mix: [],
-    mountain: ["bernese", "swiss"],
-    newfoundland: [],
-    otterhound: [],
-    ovcharka: ["caucasian"],
-    papillon: [],
-    pekinese: [],
-    pembroke: [],
-    pinscher: ["miniature"],
-    pitbull: [],
-    pointer: ["german", "germanlonghair"],
-    pomeranian: [],
-    poodle: ["miniature", "standard", "toy"],
-    pug: [],
-    puggle: [],
-    pyrenees: [],
-    redbone: [],
-    retriever: ["chesapeake", "curly", "flatcoated", "golden"],
-    ridgeback: ["rhodesian"],
-    rottweiler: [],
-    saluki: [],
-    samoyed: [],
-    schipperke: [],
-    schnauzer: ["giant", "miniature"],
-    setter: ["english", "gordon", "irish"],
-    sheepdog: ["english", "shetland"],
-    shiba: [],
-    shihtzu: [],
-    spaniel: [
-      "blenheim",
-      "brittany",
-      "cocker",
-      "irish",
-      "japanese",
-      "sussex",
-      "welsh",
-    ],
-    springer: ["english"],
-    stbernard: [],
-    terrier: [
-      "american",
-      "australian",
-      "bedlington",
-      "border",
-      "cairn",
-      "dandie",
-      "fox",
-      "irish",
-      "kerryblue",
-      "lakeland",
-      "norfolk",
-      "norwich",
-      "patterdale",
-      "russell",
-      "scottish",
-      "sealyham",
-      "silky",
-      "tibetan",
-      "toy",
-      "welsh",
-      "westhighland",
-      "wheaten",
-      "yorkshire",
-    ],
-    tervuren: [],
-    vizsla: [],
-    waterdog: ["spanish"],
-    weimaraner: [],
-    whippet: [],
-    wolfhound: ["irish"],
-  },
-  status: "success",
+const LIST_ALL_BREEDS_ENDPOINT = "https://dog.ceo/api/breeds/list/all";
+
+const fetchJSON = async (endpoint) =>
+  await fetch(endpoint).then((x) => x.json());
+
+const getRandomItemFromArray = (array) =>
+  array[Math.floor(Math.random() * array.length)];
+
+const DogImage = ({ breed }) => {
+  const { data } = useSWR(
+    `https://dog.ceo/api/breed/${breed}/images/random`,
+    fetchJSON
+  );
+
+  if (!data?.message) return null;
+  return <img src={data.message} alt={breed} />;
 };
 
 const App = () => {
+  const { data } = useSWR(LIST_ALL_BREEDS_ENDPOINT, fetchJSON);
   const [selectedBreed, setSelectedBreed] = useState(null);
-  const listOfBreeds = Object.keys(DATA.message);
+
+  if (!data?.message) return null;
+
+  const listOfBreeds = Object.keys(data.message);
+
+  const selectRandomBreed = () =>
+    setSelectedBreed(getRandomItemFromArray(listOfBreeds));
+
   return (
     <div>
-      {selectedBreed}
-      <ul>
-        {listOfBreeds.map((breed) => (
-          <li>
-            <button onClick={() => setSelectedBreed(breed)}>{breed}</button>
-          </li>
-        ))}
-      </ul>
+      <div>
+        <select
+          value={selectedBreed}
+          onChange={(e) => setSelectedBreed(e.target.value)}
+        >
+          <option value={null}>---</option>
+          {listOfBreeds.map((breed) => (
+            <option value={breed}>{breed}</option>
+          ))}
+        </select>
+        <button onClick={selectRandomBreed}>random</button>
+      </div>
+      {selectedBreed && <DogImage breed={selectedBreed} />}
     </div>
   );
 };

9fff6bbccfe1fd45c892cad9218d73e5b78face5
Vitor Dino <ext.vdino@riotgames.com>
2022-02-23 15:27:51 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,161 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const DATA = {
+  message: {
+    affenpinscher: [],
+    african: [],
+    airedale: [],
+    akita: [],
+    appenzeller: [],
+    australian: ["shepherd"],
+    basenji: [],
+    beagle: [],
+    bluetick: [],
+    borzoi: [],
+    bouvier: [],
+    boxer: [],
+    brabancon: [],
+    briard: [],
+    buhund: ["norwegian"],
+    bulldog: ["boston", "english", "french"],
+    bullterrier: ["staffordshire"],
+    cattledog: ["australian"],
+    chihuahua: [],
+    chow: [],
+    clumber: [],
+    cockapoo: [],
+    collie: ["border"],
+    coonhound: [],
+    corgi: ["cardigan"],
+    cotondetulear: [],
+    dachshund: [],
+    dalmatian: [],
+    dane: ["great"],
+    deerhound: ["scottish"],
+    dhole: [],
+    dingo: [],
+    doberman: [],
+    elkhound: ["norwegian"],
+    entlebucher: [],
+    eskimo: [],
+    finnish: ["lapphund"],
+    frise: ["bichon"],
+    germanshepherd: [],
+    greyhound: ["italian"],
+    groenendael: [],
+    havanese: [],
+    hound: [
+      "afghan",
+      "basset",
+      "blood",
+      "english",
+      "ibizan",
+      "plott",
+      "walker",
+    ],
+    husky: [],
+    keeshond: [],
+    kelpie: [],
+    komondor: [],
+    kuvasz: [],
+    labradoodle: [],
+    labrador: [],
+    leonberg: [],
+    lhasa: [],
+    malamute: [],
+    malinois: [],
+    maltese: [],
+    mastiff: ["bull", "english", "tibetan"],
+    mexicanhairless: [],
+    mix: [],
+    mountain: ["bernese", "swiss"],
+    newfoundland: [],
+    otterhound: [],
+    ovcharka: ["caucasian"],
+    papillon: [],
+    pekinese: [],
+    pembroke: [],
+    pinscher: ["miniature"],
+    pitbull: [],
+    pointer: ["german", "germanlonghair"],
+    pomeranian: [],
+    poodle: ["miniature", "standard", "toy"],
+    pug: [],
+    puggle: [],
+    pyrenees: [],
+    redbone: [],
+    retriever: ["chesapeake", "curly", "flatcoated", "golden"],
+    ridgeback: ["rhodesian"],
+    rottweiler: [],
+    saluki: [],
+    samoyed: [],
+    schipperke: [],
+    schnauzer: ["giant", "miniature"],
+    setter: ["english", "gordon", "irish"],
+    sheepdog: ["english", "shetland"],
+    shiba: [],
+    shihtzu: [],
+    spaniel: [
+      "blenheim",
+      "brittany",
+      "cocker",
+      "irish",
+      "japanese",
+      "sussex",
+      "welsh",
+    ],
+    springer: ["english"],
+    stbernard: [],
+    terrier: [
+      "american",
+      "australian",
+      "bedlington",
+      "border",
+      "cairn",
+      "dandie",
+      "fox",
+      "irish",
+      "kerryblue",
+      "lakeland",
+      "norfolk",
+      "norwich",
+      "patterdale",
+      "russell",
+      "scottish",
+      "sealyham",
+      "silky",
+      "tibetan",
+      "toy",
+      "welsh",
+      "westhighland",
+      "wheaten",
+      "yorkshire",
+    ],
+    tervuren: [],
+    vizsla: [],
+    waterdog: ["spanish"],
+    weimaraner: [],
+    whippet: [],
+    wolfhound: ["irish"],
+  },
+  status: "success",
+};
+
+const App = () => {
+  const [selectedBreed, setSelectedBreed] = useState(null);
+  const listOfBreeds = Object.keys(DATA.message);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {selectedBreed}
+      <ul>
+        {listOfBreeds.map((breed) => (
+          <li>
+            <button onClick={() => setSelectedBreed(breed)}>{breed}</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
